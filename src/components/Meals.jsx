6346b1923db2ed9f5fb565e9b3d72928f2503f10
shafiquddin <shafiquddin2k@gmail.com>
2024-06-13 21:46:13 +0530

    useHttp

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
+const requestConfig = {};
 const Meals = () => {
-    const [loadedMeals,setLoadedMeals] = useState([])
-    useEffect(()=>{
-        const fetchMeals = async() => {
-            const response = await fetch('http://localhost:3000/meals');
-            if(!response.ok){
-                //...
-            }
-            const meals = await response.json();
-            setLoadedMeals(meals)
-        }
-        fetchMeals();
-    },[])
+
+   const {data:loadedMeals,isLoading,error} = useHttp('http://localhost:3000/meals',requestConfig,[]);
+
+   if(isLoading){
+    return <p>Meals are loading</p>
+   }
+
     return <ul id='meals'>
         {loadedMeals.map(meal=>{
             return <MealItem key={meal.id} meal={meal}/>
---
+++ src/hooks/useHttp.js
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from "react";
+
+const sendHttpRequest = async (url,config) => {
+    const response = await fetch(url,config);
+    const resData = await response.json();
+    if(!response.ok){
+        throw new Error(resData.message || 'something went wrong, failed to send request.')
+    }
+    return resData;
+}
+
+const useHttp = (url,config,initialState) => {
+    const [error,setError] = useState();
+    const [data,setData] = useState(initialState);
+    const [isLoading,setIsLoading] = useState();
+
+    const sendRequest = useCallback(async()=>{
+        setIsLoading(true);
+        try{
+            const resData = await sendHttpRequest(url,config);
+            setData(resData);
+        }catch(error){
+            setError(error.message || 'somthing went wrong!')
+        }
+        setIsLoading(false);
+    },[url,config])
+
+    useEffect(()=>{
+        if((config && (config.method === "GET" || !config.method)) || !config){
+            sendRequest();
+        }
+    },[sendRequest,config])
+
+    return{
+        error,
+        data,
+        isLoading,
+        sendRequest
+    }
+}
+
+export default useHttp;
