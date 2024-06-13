633516c24fb1493b258dcfbeea98e17435e2ca4d
shafiquddin <shafiquddin2k@gmail.com>
2024-06-13 22:55:35 +0530

    Error added

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
+import Error from "./UI/Error";
 
 const requestConfig = {};
 const Meals = () => {
@@ -7,7 +8,11 @@ const Meals = () => {
    const {data:loadedMeals,isLoading,error} = useHttp('http://localhost:3000/meals',requestConfig,[]);
 
    if(isLoading){
-    return <p>Meals are loading</p>
+    return <p className="center">Meals are loading</p>
+   }
+
+   if(error){
+    return <Error title="Failed to fetch" message={error}/>
    }
 
     return <ul id='meals'>
---
+++ src/components/UI/Error.jsx
@@ -0,0 +1,9 @@
+
+const Error = ({title,message}) => {
+    return <div className="error">
+        <h2>{title}</h2>
+        <p>{message}</p>
+    </div>
+}
+
+export default Error;
